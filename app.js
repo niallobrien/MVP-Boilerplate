4330c4b9540721f21758488a4d0a39103ec6712c
Niall O'Brien <me@niallobrien.me>
2015-11-18 20:51:56 +0000

    Improves the router setup

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./app/main/indexRoute');
-var user = require('./app/components/user/userRoute');
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    index = require('./app/router'),
+    user = require('./app/components/user/userRoute'),
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app/main'));
@@ -21,9 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Setup router
 app.use('/', index);
-app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/users', function(req, res, next) {
+  res.render('index', { title: 'Express Users' });
+});
+
 module.exports = router;
